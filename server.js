31886182b267a3c10974c2a3839c5543bfec62ae
Ben Hopkins <bjhops17@gmail.com>
2021-08-26 12:34:20 -0700

    initial run starting now

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
-const child_process = require('child_process'); // native in nodeJs
-
-
-
-
-
-var copyRecursiveSync = function (src, dest) {
+const child_process = require("child_process"); // native in nodeJs
+const moment = require("moment");
+const choices = [
+  "01-HTML-Git-CSS",
+  "02-Advanced-CSS",
+  "03-JavaScript",
+  "04-Web-APIs",
+  "05-Third-Party-APIs",
+  "06-Server-Side-APIs",
+  "07-Project-1",
+  "08-Project-1",
+  "09-NodeJS",
+  "10-OOP",
+  "11-Express",
+  "12-SQL",
+  "13-ORM",
+  "14-MVC",
+  "15-Project-2",
+  "16-Project-2",
+  "17-CS",
+  "18-NoSQL",
+  "19-PWA",
+  "20-React",
+  "21-MERN",
+  "22-State",
+  "23-Project-3",
+  "24-Project-3",
+];
+const PathToLocalVersionOfGitLab =
+  "/Users/benhopkins/code/uncc-virt-bo-fsf-pt-07-2021-u-b";
+// Getting list of current folders in GitLab
+let ArrayOfFolders = fs
+  .readdirSync(PathToLocalVersionOfGitLab)
+  .filter(function (file) {
+    return fs.statSync(PathToLocalVersionOfGitLab + "/" + file).isDirectory();
+  });
+//Getting value of next index that needs to be pushed
+const nextIndex = parseInt(
+  ArrayOfFolders[ArrayOfFolders.length - 1].split("-")[0]
+);
+console.log(nextIndex);
+// Function for copying every file over that is not in Solved Main or main
+function copyRecursiveSync(src, dest) {
   try {
     var exists = fs.existsSync(src);
     var stats = exists && fs.statSync(src);
@@ -30,59 +66,35 @@ var copyRecursiveSync = function (src, dest) {
     } else {
       fs.copyFileSync(src, dest);
     }
-  
   } catch (err) {
     console.error(err.message);
   }
-};
-inquirer
-  .prompt([
-    {
-      name: "data",
-      message: "What module would you like to upload to GitLab?",
-      type: "list",
-      choices: [
-        "01-HTML-Git-CSS",
-        "02-Advanced-CSS",
-        "03-JavaScript",
-        "04-Web-APIs",
-        "05-Third-Party-APIs",
-        "06-Server-Side-APIs",
-        "09-NodeJS",
-        "10-OOP",
-        "11-Express",
-        "12-SQL",
-        "13-ORM",
-        "14-MVC",
-        "17-CS",
-        "18-NoSQL",
-        "19-PWA",
-        "20-React",
-        "21-MERN",
-        "22-State",
-      ],
-    },
-  ])
-  .then(async(res) => {
-    await copyRecursiveSync(
-      `/Users/benhopkins/class/fullstack-online/01-Class-Content/${res.data}`,
-      `/Users/benhopkins/class/test/test-script/${res.data}`
-    );
-    
-  let child = child_process.exec('git add . && git commit -m "Initial commit" && git push', { cwd: '/Users/benhopkins/class/test/test-script'})
-   child.stderr.on('data', function (data) {
-    console.error("STDERR:", data.toString());
-  });
-   child.stderr.on('error', function (data) {
-    console.error("test", data.toString());
-  });
-  child.stdout.on('data', function (data) {
-    console.log("STDOUT:", data.toString());
-  });
-  child.on('exit', function (exitCode) {
-    console.log("Child exited with code: " + exitCode);
-  });
+}
 
-  });
-  
+setInterval(() => {
+  console.log("setInterval triggered")
+  if (moment().format("dddd") === "Friday") {
+    copyRecursiveSync(
+      `/Users/benhopkins/class/fullstack-online/01-Class-Content/${choices[nextIndex]}`,
+      `/Users/benhopkins/class/uncc-virt-bo-fsf-pt-07-2021-u-b/${choices[nextIndex]}`
+    );
+    let child = child_process.exec(
+      'git add . && git commit -m "Initial commit" && git push',
+      { cwd: "/Users/benhopkins/class/uncc-virt-bo-fsf-pt-07-2021-u-b" }
+    );
+    child.stderr.on("data", function (data) {
+      console.error("STDERR:", data.toString());
+    });
+    child.stderr.on("error", function (data) {
+      console.error("test", data.toString());
+    });
+    child.stdout.on("data", function (data) {
+      console.log("STDOUT:", data.toString());
+    });
+    child.on("exit", function (exitCode) {
+      console.log("Child exited with code: " + exitCode);
+    });
+  }
+}, 42500000);
+
 
